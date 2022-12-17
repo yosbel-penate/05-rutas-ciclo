0504ed18d40e807643b6fd1dac31da87b8c2b9b3
Corso <58090876+yosbel-penate@users.noreply.github.com>
2022-12-17 11:54:24 -0500

    hasta el video 17

---
+++ src/modules/shared/componets/CustomLink.vue
@@ -0,0 +1,45 @@
+<template>
+        <a v-if="isExternalLink"
+                :href="link.to"
+                target="_blank"
+            >
+            {{link.name}}
+        </a>
+        <router-link 
+            v-else
+            :to="link.to"
+            v-slot="{href, isActive}"
+        >
+            <a  :class="isActive ? 'isActive':'normal-link'"
+                :href="href"
+            >
+                {{link.name}}
+            </a>
+        </router-link>
+</template>
+
+<script>
+export default {
+   props:{
+    link:{
+        type: Object,
+        required: true
+    }
+   },
+   computed: {
+    isExternalLink() {
+        return this.link.to.startsWith('http');
+    },
+   
+   },
+};
+</script>
+
+<style scoped>
+.isActive{
+    color: #42b983;
+}
+.normal-link{
+    color:#c6c5c5
+}
+</style>
--- src/modules/shared/componets/NavBar.vue
+++ src/modules/shared/componets/NavBar.vue
@@ -1,10 +1,36 @@
 <template>
     <div>
-        <router-link :to="{name:'root'}">Pokemon List</router-link>
-        <router-link :to="{name: 'pokemon-id', params:{id: 85}}">Pokemon por id</router-link>
-        <router-link :to="{name: 'about'}">About</router-link>
+        <!-- <router-link :to="{name:'root'}">Pokemon List</router-link> -->
+        <!-- <router-link :to="{name: 'pokemon-id', params:{id: 85}}">Pokemon por id</router-link> -->
+        <!-- <router-link :to="{name: 'about'}">About</router-link> -->
+        <CustomLink 
+            v-for="link in links"
+            :key="link.to"
+            :link="link"
+        />
     </div>
 </template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+
+export default {
+    data() {
+        return {
+            links: [
+                {to:'/', name: 'Pokemon'},
+                {to:'/50', name: 'Por ID'},
+                {to:'/about', name: 'About'},
+                {to:'https://google.com', name: 'Google'}
+            ],
+        };
+    },
+    components: {
+         CustomLink: defineAsyncComponent(() => import( /*webpackChunkName: "CustomLink" */ './CustomLink.vue'))
+    },
+};
+</script>
+
 <style scoped>
     div{
         padding: 0 30px;
@@ -13,8 +39,5 @@
         font-weight: bold;
         color: #2c3e50;
         margin: 0 10px;
-    }
-    a.router-link-exact-active{
-        color: #42b983
     }
 </style>
